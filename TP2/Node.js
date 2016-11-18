12483cb6a3ded157aeb1d8a1229b07f5a288c36e
José Carlos <jose__carlos17@hotmail.com>
2016-11-18 17:55:10 +0000

    Added new scene (to show chessboards)

--- TP2/Node.js
+++ TP2/Node.js
@@ -9,6 +9,7 @@ function Node(id) {
     this.animations = [];
     this.activeAnimation = 0;
     this.activeShader = null;       //null to avoid shaders, int to select shader
+    this.primitiveWithShaderInfo = null;
 };
 
 Node.prototype.pushMaterial = function (material) {
@@ -48,3 +49,30 @@ Node.prototype.getPrimitive = function () {
 Node.prototype.pushAnimation = function (animation) {
     this.animations.push(animation);
 };
+
+Node.prototype.setupShaders = function (scene) {
+    scene.setActiveShader(scene.testShaders[this.activeShader]);
+    this.texture.bind(this.activeShader);
+
+    if (this.activeShader == 0) {
+        scene.testShaders[0].setUniformsValues({ dimX: this.primitiveWithShaderInfo.partsX });
+        scene.testShaders[0].setUniformsValues({ dimY: this.primitiveWithShaderInfo.partsY });
+        scene.testShaders[0].setUniformsValues({ selectedU: this.primitiveWithShaderInfo.selectedU });
+        scene.testShaders[0].setUniformsValues({ selectedV: this.primitiveWithShaderInfo.selectedV });
+
+        scene.testShaders[0].setUniformsValues({ c1R: this.primitiveWithShaderInfo.c1R });
+        scene.testShaders[0].setUniformsValues({ c1G: this.primitiveWithShaderInfo.c1G });
+        scene.testShaders[0].setUniformsValues({ c1B: this.primitiveWithShaderInfo.c1B });
+        scene.testShaders[0].setUniformsValues({ c1A: this.primitiveWithShaderInfo.c1A });
+
+        scene.testShaders[0].setUniformsValues({ c2R: this.primitiveWithShaderInfo.c2R });
+        scene.testShaders[0].setUniformsValues({ c2G: this.primitiveWithShaderInfo.c2G });
+        scene.testShaders[0].setUniformsValues({ c2B: this.primitiveWithShaderInfo.c2B });
+        scene.testShaders[0].setUniformsValues({ c2A: this.primitiveWithShaderInfo.c2A });
+
+        scene.testShaders[0].setUniformsValues({ csR: this.primitiveWithShaderInfo.csR });
+        scene.testShaders[0].setUniformsValues({ csG: this.primitiveWithShaderInfo.csG });
+        scene.testShaders[0].setUniformsValues({ csB: this.primitiveWithShaderInfo.csB });
+        scene.testShaders[0].setUniformsValues({ csA: this.primitiveWithShaderInfo.csA });
+    }
+}
