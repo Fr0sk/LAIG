02b8d9054b96e6029d41c2a7a66a7c0d000cf0ca
José Carlos <jose__carlos17@hotmail.com>
2016-12-20 00:55:01 +0000

    Can get the cells ids of the ai piece to play

--- TP3/Game.js
+++ TP3/Game.js
@@ -12,7 +12,7 @@ function Game(scene) {
 Game.prototype = Object.create(CGFobject.prototype);
 Game.prototype.constructor = Game;
 
-Game.prototype.startGame = function() {
+Game.prototype.startGame = function () {
     this.ships = []; // Gets initialized with board
     this.board = new Board(this.scene, this, "idBoard");
     this.player = 1;
@@ -20,7 +20,7 @@ Game.prototype.startGame = function() {
     prologBoard = this.board.toString();
 }
 
-Game.prototype.picking = async function(obj, id) {
+Game.prototype.picking = async function (obj, id) {
     var selected = false;
     if (!this.selectedShip) {
         for (var s = 0; s < this.ships.length; s++) {
@@ -40,7 +40,7 @@ Game.prototype.picking = async function(obj, id) {
                 this.callRequest(prologRequestUser, this.handleReplyBoard);
                 await sleep(1000);
                 updatedPrologBoard = serverResponse;
-                if(updatedPrologBoard.substring(0, 3) !=  '[[[') {
+                if (updatedPrologBoard.substring(0, 3) != '[[[') {
                     console.error("Prolog Error!");
                     return;
                 }
@@ -53,25 +53,9 @@ Game.prototype.picking = async function(obj, id) {
                 await sleep(1000);
                 prologBoard = serverResponse;
 
-                var userSplit = updatedPrologBoard.split(']],');
-                var aiSplit = prologBoard.split(']],');
-
-                var index;
-                var shipPosX;
-                var shipPosY;
-                for(var row = 0; row < userSplit.length; row++) {
-                    if(aiSplit[row].indexOf(aiShip) != -1) {
-                        aiSplitRow = aiSplit[row].split('],[');
-                        for(var column = 0; column < aiSplitRow.length; column++) {
-                            if(aiSplitRow[column].indexOf(aiShip) != -1) {
-                                shipPosX = column;
-                                shipPosY = row;
-                                break;
-                            }
-                        }
-                        break;
-                    }
-                }
+                this.setOriginCellId(updatedPrologBoard);
+                this.setDestinationCellId(updatedPrologBoard, prologBoard);
+                console.warn(this.originCellId, this.destinationCellId);
 
                 this.doMove(obj);
             }
@@ -82,29 +66,69 @@ Game.prototype.picking = async function(obj, id) {
     }
 }
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+Game.prototype.setOriginCellId = function (userBoard) {
+    var userSplit = userBoard.split(']],');
+
+    for (var row = 0; row < userSplit.length; row++) {
+        if (userSplit[row].indexOf(aiShip) != -1) {
+            userSplitRow = userSplit[row].split('],[');
+            for (var column = 0; column < userSplitRow.length; column++) {
+                if (userSplitRow[column].indexOf(aiShip) != -1) {
+                    this.originCellId = row * userSplitRow.length + column + 1;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    return true;
 }
 
-Game.prototype.doMove = function(toCell) {
+Game.prototype.setDestinationCellId = function (userBoard, aiBoard) {
+    var userSplit = userBoard.split(']],');
+    var aiSplit = aiBoard.split(']],');
+
+    var index;
+    for (var row = 0; row < userSplit.length; row++) {
+        if (aiSplit[row].indexOf(aiShip) != -1) {
+            aiSplitRow = aiSplit[row].split('],[');
+            for (var column = 0; column < aiSplitRow.length; column++) {
+                if (aiSplitRow[column].indexOf(aiShip) != -1) {
+                    this.destinationCellId = row * aiSplitRow.length + column + 1;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    return true;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+Game.prototype.doMove = function (toCell) {
     var fromCell = this.selectedShip.cell;
     toCell.moveShip(this.selectedShip, true);
     this.moveStack.push({ from: fromCell, to: toCell });
     this.nextPlayer();
 }
 
-Game.prototype.nextPlayer = function() {
+Game.prototype.nextPlayer = function () {
     this.player = this.player == 1 ? 2 : 1;
     console.log("Player " + this.player + ", it's your turn!");
 }
 
-Game.prototype.update = function(deltaTime) {
+Game.prototype.update = function (deltaTime) {
     if (this.ships)
-        for (var s  = 0; s < this.ships.length; s++) 
+        for (var s = 0; s < this.ships.length; s++)
             this.ships[s].update(deltaTime);
 }
 
-Game.prototype.getMovementDirection = function(fromCellId, toCellId) {
+Game.prototype.getMovementDirection = function (fromCellId, toCellId) {
     //Getting the row fromCell belongs
     var fromCellRow = this.getRow(fromCellId, this.board.rowLength, this.board.board.length * this.board.rowLength);
     var toCellRow = this.getRow(toCellId, this.board.rowLength, this.board.board.length * this.board.rowLength);
@@ -118,8 +142,8 @@ Game.prototype.getMovementDirection = function(fromCellId, toCellId) {
 
     id = fromCellId;
     idRow = fromCellRow;
-    for(var i = 0; i < rowsDifference; i++) {
-        if(idRow % 2 == 0)
+    for (var i = 0; i < rowsDifference; i++) {
+        if (idRow % 2 == 0)
             id += this.board.rowLength;
         else
             id += this.board.rowLength + 1;
@@ -129,8 +153,8 @@ Game.prototype.getMovementDirection = function(fromCellId, toCellId) {
 
     id = fromCellId;
     idRow = fromCellRow;
-    for(var i = 0; i < rowsDifference; i++) {
-        if(idRow % 2 == 0)
+    for (var i = 0; i < rowsDifference; i++) {
+        if (idRow % 2 == 0)
             id += this.board.rowLength - 1;
         else
             id += this.board.rowLength;
@@ -140,8 +164,8 @@ Game.prototype.getMovementDirection = function(fromCellId, toCellId) {
 
     id = fromCellId;
     idRow = fromCellRow;
-    for(var i = 0; i < rowsDifference; i++) {
-        if(idRow % 2 == 0)
+    for (var i = 0; i < rowsDifference; i++) {
+        if (idRow % 2 == 0)
             id -= this.board.rowLength;
         else
             id -= this.board.rowLength - 1;
@@ -151,8 +175,8 @@ Game.prototype.getMovementDirection = function(fromCellId, toCellId) {
 
     id = fromCellId;
     idRow = fromCellRow;
-    for(var i = 0; i < rowsDifference; i++) {
-        if(idRow % 2 == 0)
+    for (var i = 0; i < rowsDifference; i++) {
+        if (idRow % 2 == 0)
             id -= this.board.rowLength + 1;
         else
             id -= this.board.rowLength;
@@ -168,7 +192,7 @@ Game.prototype.getMovementDirection = function(fromCellId, toCellId) {
     id += this.board.rowLength * rowsDifference;
     var resultS = id;
 
-    switch(toCellId) {
+    switch (toCellId) {
         case resultSE: this.direction = 'se'; break;
         case resultSW: this.direction = 'sw'; break;
         case resultNE: this.direction = 'ne'; break;
@@ -179,12 +203,12 @@ Game.prototype.getMovementDirection = function(fromCellId, toCellId) {
     }
 }
 
-Game.prototype.getRow = function(id, rowLength, totalNumIds) {
+Game.prototype.getRow = function (id, rowLength, totalNumIds) {
     var idCellRow;
     var currRow = 0;
 
-    for(var currId = rowLength; currId <= totalNumIds; currId += rowLength) {
-        if(id <= currId)
+    for (var currId = rowLength; currId <= totalNumIds; currId += rowLength) {
+        if (id <= currId)
             return currRow;
         currRow++;
     }
@@ -192,29 +216,29 @@ Game.prototype.getRow = function(id, rowLength, totalNumIds) {
     return null;
 }
 
-Game.prototype.display = function() {
+Game.prototype.display = function () {
     this.board.display();
 }
 
-Game.prototype.callRequest = function(requestString, onSuccess, onError, port) {
+Game.prototype.callRequest = function (requestString, onSuccess, onError, port) {
     var requestPort = port || 8081;
     var request = new XMLHttpRequest();
     request.open('GET', 'http://localhost:' + requestPort + '/' + requestString, true);
 
-    request.onload = onSuccess || function(data){console.log("Request successful.");};
-    request.onerror = onError || function(){console.log("Error waiting for response");};
+    request.onload = onSuccess || function (data) { console.log("Request successful."); };
+    request.onerror = onError || function () { console.log("Error waiting for response"); };
 
     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
     request.send();
 }
 
-Game.prototype.handleReplyBoard = function(data) {
+Game.prototype.handleReplyBoard = function (data) {
     console.info("Response: " + data.target.response);
     serverResponse = data.target.response;
     connectionBoolean = true;
 }
 
-Game.prototype.handleReplyShip = function(data) {
+Game.prototype.handleReplyShip = function (data) {
     console.info("Response: " + data.target.response);
     aiShip = data.target.response;
     connectionBoolean = true;
