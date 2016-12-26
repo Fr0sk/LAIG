e5cc1377df954de00cc73d402e22d9b3ed0cb546
Fr0sk <the.fr0sk@gmail.com>
2016-12-26 10:47:40 +0000

    Added textures to ships

--- TP3/Ship.js
+++ TP3/Ship.js
@@ -4,6 +4,9 @@ function Ship(scene, id, owner) {
     this.patch = PatchBuilder.buildVehiclePatch(scene, id);
     this.owner = owner;
     this.id = id;
+    this.appearance = new CGFappearance(scene);
+    this.appearance.setAmbient(1, 1, 1, 1);
+    this.appearance.loadTexture('./resources/player' + owner + '.jpg');
 }
 
 Ship.prototype = Object.create(Node.prototype);
@@ -24,13 +27,12 @@ Ship.prototype.display = function() {
     if (this.rotate) { mat4.rotateX(mat, mat, this.rotate.x); mat4.rotateY(mat, mat, this.rotate.y); mat4.rotateZ(mat, mat, this.rotate.z); }
     if (this.scale) mat4.scale(mat, mat, [this.scale.x, this.scale.y, this.scale.z]);
     this.scene.pushMatrix();
+        this.appearance.apply();
         this.scene.multMatrix(mat)
         this.scene.pushMatrix();
-            //this.scene.translate(0, 1, 0);
             this.patch.display();
         this.scene.popMatrix();
         this.scene.pushMatrix();
-            //this.scene.translate(0, 1, 0);
             this.scene.rotate(Math.PI, 1, 0, 0);
             this.patch.display();
         this.scene.popMatrix();
